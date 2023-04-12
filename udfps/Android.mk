itzryanhere/android_device_samsung_a50

a50-common: Add UdfpsHandler for NightDisplay and ExtraDim
* Optical fingerprint sensors don't like NightDisplay and ExtraDim since those decrease overall sensor accuracy
* Turn ExtraDim off and set NightDisplay's color temperature to AOSP's default white balance value if the fingerprint sensor is touched

Change-Id: I6651cd5c8c066102feebfb764184b099c6ad741d

udfps/Android.mk
@@ -1,0 +1,20 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := SamsungUdfpsHandler.a50
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_USE_AAPT2 := true
+
+ifneq ($(INCREMENTAL_BUILDS),)
+    LOCAL_PROGUARD_ENABLED := disabled
+    LOCAL_JACK_ENABLED := incremental
+endif
+
+include $(BUILD_PACKAGE)

Matti67/android_device_samsung_a505f

udfps/Android.mk
@@ -1,0 +1,20 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := SamsungUdfpsHandler.universal9611
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_USE_AAPT2 := true
+
+ifneq ($(INCREMENTAL_BUILDS),)
+    LOCAL_PROGUARD_ENABLED := disabled
+    LOCAL_JACK_ENABLED := incremental
+endif
+
+include $(BUILD_PACKAGE)