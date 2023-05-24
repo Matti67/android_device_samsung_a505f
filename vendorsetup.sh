d40dc1316f028f2ecec3b3e9e78edfd9c770d068
Vhmit <praxedes.9630@gmail.com>
2023-05-24 13:57:15 -0300

    a505f: Update scripts dependencies
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,27 @@
-git clone https://github.com/GiovanYCringe/kernel_a50 kernel/samsung/a50;
+# Clone/Fetch Upstream Device Dependencies
+
+# Vendor tree Unified
+echo "Cloning Vendor Tree"
+git clone https://github.com/GiovanYCringe/android_vendor_samsung_a505f vendor/samsung/a505f
+echo ""
+
+# Dependencies
+echo "Cloning Dependencies Samsung"
 rm -rf hardware/samsung;
 git clone https://github.com/PixelExperience/hardware_samsung hardware/samsung;
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt;
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal;
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib;
-git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy;
+git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy
+echo ""
+
+# Framework
+echo "Patching Framework"
+rm frameworks/base/core/java/android/os/Build.java && cp device/samsung/a505f/patch/Build.java frameworks/base/core/java/android/os
+echo ""
+
+# Kernel
+echo "Cloning Clang + Kernel"
 git clone https://github.com/moto-common/platform_prebuilts_clang_host_linux-x86_clang-r383902 prebuilts/clang/host/linux-x86/clang-r383902;
-rm frameworks/base/core/java/android/os/Build.java && cp device/samsung/a50-common/patch/Build.java frameworks/base/core/java/android/os
+git clone https://github.com/GiovanYCringe/kernel_a50 kernel/samsung/a50
+echo ""
