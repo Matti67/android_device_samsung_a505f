cd20860475dc455a5aa4f6bffe667ee7d7f63ba4
Matti67 <m.mattioli67@gmail.com>
2024-03-25 16:41:18 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,11 +8,11 @@ echo ""
 # Dependencies
 echo "Cloning Dependencies Samsung"
 rm -rf hardware/samsung;
-git clone https://github.com/PixelExperience/hardware_samsung hardware/samsung;
-git clone https://github.com/LineageOS/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt;
-git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal;
-git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib;
-git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy
+git clone -b thirteen --single-branch https://github.com/PixelExperience/hardware_samsung hardware/samsung;
+git clone -b lineage-20 --single-branch https://github.com/LineageOS/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt;
+git clone -b lineage-20 --single-branch https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal;
+git clone -b lineage-20 --single-branch https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib;
+git clone -b lineage-20 --single-branch https://github.com/LineageOS/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy
 echo ""
 
 # Framework
@@ -22,6 +22,6 @@ echo ""
 
 # Kernel
 echo "Cloning Clang + Kernel"
-git clone https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone -b 11 prebuilts/clang/host/linux-x86/clang-r399163b1;
-git clone https://github.com/GiovanYCringe/kernel_a50 kernel/samsung/a50
+#git clone https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone -b 11 prebuilts/clang/host/linux-x86/clang-r399163b1;
+git clone -b Lineage-20 --single-branch https://github.com/Matti67/kernel_a50 kernel/samsung/a50
 echo ""
